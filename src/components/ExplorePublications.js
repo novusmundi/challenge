532e7be89954fa4ce02654a9a0e2333cab4df5f3
Francisco Rey <reyf1998@outlook.es>
2023-01-18 09:54:15 -0300

    test conection

--- src/components/ExplorePublications.js
+++ src/components/ExplorePublications.js
@@ -1,31 +1,31 @@
-import React, {useEffect, useState} from 'react'
-import { explorePublications } from '../lensQueries/explorePublications'
+import React, { useEffect, useState } from "react";
+import { explorePublications } from "../lensQueries/explorePublications";
 
-export default function ExplorePublications(props){
-
-    const init = async () => {
-        try{
-            const request = {
-                sortCriteria: "LATEST", //You can filter by TOP_COMMENTED | TOP_COLLECTED | TOP_MIRRORED | LATEST
-                noRandomize:true,
-                sources:["5bba5781-78b5-4927-8d2f-122742817583"],
-                publicationTypes: ["POST"],
-                cursor:"{\"timestamp\":1,\"offset\":0}", 
-                limit:24
-              }
-            const response = await explorePublications(request) // To get next result replace the cursor with the value of response.pageInfo.next 
-            console.log(response)
-        }catch(err){
-            console.log(err)
-        }
+export default function ExplorePublications(props) {
+  const init = async () => {
+    try {
+      const request = {
+        sortCriteria: "LATEST", //You can filter by TOP_COMMENTED | TOP_COLLECTED | TOP_MIRRORED | LATEST
+        noRandomize: true,
+        sources: ["5bba5781-78b5-4927-8d2f-122742817583"],
+        publicationTypes: ["POST"],
+        cursor: '{"timestamp":1,"offset":0}',
+        limit: 24,
+      };
+      const response = await explorePublications(request); // To get next result replace the cursor with the value of response.pageInfo.next
+      console.log(response);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    useEffect(() => {
-        init()
-    },[])
+  useEffect(() => {
+    init();
+  }, []);
 
-    return(
-        <div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Test app</h1>
+    </div>
+  );
 }
--- src/lensQueries/apollo-client.js
+++ src/lensQueries/apollo-client.js
@@ -1,46 +1,43 @@
 import {
-    ApolloClient,
-    ApolloLink,
-    from,
-    HttpLink,
-    InMemoryCache,
-  } from '@apollo/client';
-  import { onError } from '@apollo/client/link/error';
-  import fetch from 'cross-fetch';
-  const lensAPI = "https://api.lens.dev"
+  ApolloClient,
+  ApolloLink,
+  from,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+const lensAPI = "https://api.lens.dev";
 
-  const defaultOptions = {
-    watchQuery: {
-      fetchPolicy: 'no-cache',
-      errorPolicy: 'ignore',
-    },
-    query: {
-      fetchPolicy: 'no-cache',
-      errorPolicy: 'all',
-    },
-  };
-  
-  const httpLink = new HttpLink({
-    uri: lensAPI,
-    fetch,
-  });
-  
-  const errorLink = onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors)  {
-      graphQLErrors.forEach(({ message, locations, path }) => {
-        console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
-      }
-  
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
+
+const httpLink = new HttpLink({
+  uri: lensAPI,
+  fetch,
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, locations, path }) => {
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
       );
-    }
-  
-  
-    if (networkError) console.log(`[Network error]: ${networkError}`);
-  });
-  
-  
-  export const apolloClient = new ApolloClient({
-    link: from([errorLink, httpLink]),
-    cache: new InMemoryCache(),
-    defaultOptions: defaultOptions,
-  });
+    });
+  }
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+export const apolloClient = new ApolloClient({
+  link: from([errorLink, httpLink]),
+  cache: new InMemoryCache(),
+  defaultOptions: defaultOptions,
+});
